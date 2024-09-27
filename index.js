dc358d1171215cdf182506ae2c2c335113dda899
Akshay S <Akshayshinde682@gmail.com>
2024-09-27 13:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ app.post("/challenge",(req,res)=>{
 app.post("/command",(req,res) => {
   var r = req.body
   console.log(r.response_url);
+  fetch(r.response_url,{
+     headers: {
+      "Content-Type":"appplication/json"
+     },
+     "method":"POST",
+     "body":"{'response_type':'in_channel','text':'hello world'}"
+  })
   res.status(200);
   res.header("Content-Type: application/json")
-  res.send("hello world");
+  res.send("");
 })
 
 app.listen(3000,()=>{
